f861787e37705d941e28769819c5799cb043c05d
Sijo Thomas <sijo.perumadan@gmail.com>
2022-03-17 05:41:29 -0700

    Supported Mac OS.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "LightNetwork",
+    platforms: [
+            .macOS(.v10_11),
+            .iOS(.v10)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
